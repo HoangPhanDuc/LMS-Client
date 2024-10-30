f0a3210890b828cec3f889116d81ac089683b0c0
hackTran <ductv.21it@vku.udn.vn>
2024-10-30 14:44:02 +0700

    create components && constants

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          let iconName;
+          if (route.name === "index") {
+            iconName = require("@/assets/icons/HouseSimple.png");
+          } else if (route.name === "search/index") {
+            iconName = require("@/assets/icons/search.png");
+          } else if (route.name === "courses/index") {
+            iconName = require("@/assets/icons/BookBookmark.png");
+          } else if (route.name === "profile/index") {
+            iconName = require("@/assets/icons/User.png");
+          }
+          return (
+            <Image
+              style={{ width: 25, height: 25, tintColor: color }}
+              source={iconName}
+            />
+          );
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+      })}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="search/index" />
+      <Tabs.Screen name="courses/index" />
+      <Tabs.Screen name="profile/index" />
+    </Tabs>
+  );
+}
---
+++ components/search/search.input.tsx
@@ -0,0 +1,65 @@
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import React from "react";
+import { useFonts } from "expo-font";
+import { Nunito_700Bold } from "@expo-google-fonts/nunito";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function SearchInput() {
+  let [fontsLoaded, fontError] = useFonts({
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  return (
+    <View style={styles.filteringContainer}>
+      <View style={styles.searchContainer}>
+        <TextInput style={[styles.input, {fontFamily: "Nunito_700QBold"}]}
+            placeholder="Search here....."
+            placeholderTextColor={"#C67cc"}
+        />
+        <TouchableOpacity style={styles.searchIconContainer}>
+            <AntDesign name="search1" size={20} color="#fff"/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export const styles = StyleSheet.create({
+  filteringContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 16,
+  },
+
+  searchContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+
+  searchIconContainer: {
+    width: 36,
+    height: 36,
+    backgroundColor: "#2467EC",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+
+  input: {
+    flex: 1,
+    fontSize: 14,
+    color: "black",
+    paddingVertical: 10,
+    width: 271,
+    height: 48,
+  },
+});
